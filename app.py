b66ce531af0497121ccffb29000d2777fd80f556
DucV <ducvu1045@gmail.com>
2026-03-19 17:00:36 +0700

    init

--- app.py
+++ app.py
@@ -3,15 +3,20 @@ from emotion_detection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
 
+
 @app.route("/emotion", methods=["POST"])
 def detect_emotion():
     data = request.get_json()
 
-    text = data.get("text")
+    if not data or "text" not in data:
+        return jsonify({"error": "Invalid input"}), 400
+
+    text = data["text"]
 
     result = emotion_detector(text)
 
     return jsonify(result)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
