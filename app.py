1a40043ee53ede50d8249b78718163649993ab9d
DucV <ducvu1045@gmail.com>
2026-03-19 18:12:21 +0700

    commit

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", result=None)
 
 
 @app.route("/emotionDetector")
@@ -16,18 +16,16 @@ def emotion_detector_api():
     response = emotion_detector(text)
 
     if "error" in response:
-        return "Invalid text! Please try again!"
+        result = "Invalid text! Please try again!"
+    else:
+        result = (
+            f"For the given statement, the system response is "
+            f"anger: {response['anger']}, "
+            f"disgust: {response['disgust']}, "
+            f"fear: {response['fear']}, "
+            f"joy: {response['joy']} and "
+            f"sadness: {response['sadness']}. "
+            f"The dominant emotion is {response['dominant_emotion']}."
+        )
 
-    return (
-        f"For the given statement, the system response is "
-        f"anger: {response['anger']}, "
-        f"disgust: {response['disgust']}, "
-        f"fear: {response['fear']}, "
-        f"joy: {response['joy']} and "
-        f"sadness: {response['sadness']}. "
-        f"The dominant emotion is {response['dominant_emotion']}."
-    )
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    return render_template("index.html", result=result)
